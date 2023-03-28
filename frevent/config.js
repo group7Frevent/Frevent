ef072dc85fe67b6ffd337a76ab98d27112ee24ee
Kaarnis <karnarantaveeti@gmail.com>
2023-03-28 14:03:46 +0300

    rekisteröinti valmis

--- frevent/config.js
+++ frevent/config.js
@@ -4,13 +4,13 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAqvSpIYEk68CazVlP0gTv-pQZRJWqHnCQ",
-  authDomain: "mobiiliprojekti-ee10f.firebaseapp.com",
-  projectId: "mobiiliprojekti-ee10f",
-  storageBucket: "mobiiliprojekti-ee10f.appspot.com",
-  messagingSenderId: "1022912506300",
-  appId: "1:1022912506300:web:6742017a171ebaf98363b9",
-  measurementId: "G-7P4Z0JGEWS"
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
 }
 
 if (!firebase.apps.length){
